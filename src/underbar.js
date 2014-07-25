89cedfe0fade691fae220b05eee983dbbb32b9d6
zuyanliang@gmail.com <zuyanliang@gmail.com>
2014-07-25 16:04:18 -0700

    finished _extends and _defaults

--- src/underbar.js
+++ src/underbar.js
@@ -201,14 +201,29 @@ var _ = {};
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
-    //_.reduce(collection, function())
-
+    return _.reduce(collection, function(prev, current){
+      if(iterator == undefined){
+        return !!(prev) && current; 
+      }
+      else{
+        return !!(prev) && iterator(current); 
+      }
+     }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    return _.reduce(collection, function(prev, current){
+      if(iterator == undefined){
+        return !!(prev) || current; 
+      }
+      else{
+        return !!(prev) || iterator(current); 
+      }
+    });
+
   };
 
 
@@ -231,11 +246,25 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for(var i = 1; i < arguments.length; i++){
+      for(var k in arguments[i]){
+        obj[k] = arguments[i][k];
+      }
+    }
+    return obj; 
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for(var i = 1; i < arguments.length; i++){
+      for(var k in arguments[i]){
+          if(obj[k] === undefined){
+          obj[k] = arguments[i][k];
+        }
+      }
+    }
+    return obj; 
   };
 
 
